570123322bce8ecdfbf4d3364b7b22e5c9655aee
Jamie English <jamienglish@gmail.com>
2012-04-21 18:26:47 +0100

    Refactoring: Replace conditional with polymorphism on Orientation

--- lib/mars_rover/orientation.rb
+++ lib/mars_rover/orientation.rb
@@ -2,38 +2,83 @@ module MarsRover
   class InvalidOrientationError < StandardError; end
 
   class Orientation
-    attr_reader :direction
-
-    VALID_ORIENTATIONS = %w(N E S W)
-
     def initialize(direction)
-      raise InvalidOrientationError unless valid_orientation?(direction)
-
-      @direction = direction
+      klass = case direction
+              when 'N' then North
+              when 'E' then East
+              when 'S' then South
+              when 'W' then West
+              else raise InvalidOrientationError
+              end
+      @direction = klass.new
     end
 
     def turn_right
-      @direction = case @direction
-                   when 'N' then 'E'
-                   when 'E' then 'S'
-                   when 'S' then 'W'
-                   when 'W' then 'N'
-                   end
+      @direction = @direction.turn_right
     end
 
     def turn_left
-      @direction = case @direction
-                   when 'N' then 'W'
-                   when 'E' then 'N'
-                   when 'S' then 'E'
-                   when 'W' then 'S'
-                   end
+      @direction = @direction.turn_left
+    end
+
+    def direction
+      @direction.to_s
     end
     
-    private
+    class North
+      def turn_right
+        East.new
+      end
 
-    def valid_orientation?(orientation)
-      VALID_ORIENTATIONS.include?(orientation)
+      def turn_left
+        West.new
+      end
+
+      def to_s
+        'N'
+      end
+    end
+
+    class East
+      def turn_right
+        South.new
+      end
+
+      def turn_left
+        North.new
+      end
+
+      def to_s
+        'E'
+      end
+    end
+
+    class South
+      def turn_right
+        West.new
+      end
+
+      def turn_left
+        East.new
+      end
+
+      def to_s
+        'S'
+      end
+    end
+
+    class West
+      def turn_right
+        North.new
+      end
+
+      def turn_left
+        South.new
+      end
+
+      def to_s
+        'W'
+      end
     end
   end
 end
